ee9674e1d38cb5ece08474dff8fe93a0edd27d8b
Parham Alvani <parham.alvani@gmail.com>
2016-03-05 19:43:25 +0330

    Correct naming issues.

--- HW-1/src/p4-1-2/p4-2.vhd
+++ HW-1/src/p4-1-2/p4-2.vhd
@@ -19,25 +19,25 @@ architecture structural of sixteen_bit_comparator is
 	signal a2_b2_eq, a2_b2_gt, a2_b2_lt : std_logic;
 	signal a3_b3_eq, a3_b3_gt, a3_b3_lt : std_logic;
 
-	entity 4_bit_comparator is
+	entity four_bit_comparator is
 		port (a, b : in std_logic_vector(3 downto 0);
 			l, g, e : in std_logic;
 			eq, gt, lt : out std_logic);
-	end entity 4_bit_comparator;
+	end entity four_bit_comparator;
 
 
-	for all:4_bit_comparator use entity work.4_bit_comparator;
+	for all:four_bit_comparator use entity work.four_bit_comparator;
 begin
-	c0: 4_bit_comparator port map (a(3 downto 0), b(3 downto 0), open, open, open,
+	c0: four_bit_comparator port map (a(3 downto 0), b(3 downto 0), open, open, open,
 		a0_b0_eq, a0_b0_gt, a0_b0_lt);
 
-	c1: 4_bit_comparator port map (a(7 downto 4), b(7 downto 4), open, open, open,
+	c1: four_bit_comparator port map (a(7 downto 4), b(7 downto 4), open, open, open,
 		a1_b1_eq, a1_b1_gt, a1_b1_lt);
 
-	c2: 4_bit_comparator port map (a(11 downto 8), b(11 downto 8), open, open, open,
+	c2: four_bit_comparator port map (a(11 downto 8), b(11 downto 8), open, open, open,
 		a2_b2_eq, a2_b2_gt, a2_b2_lt);
 
-	c3: 4_bit_comparator port map (a(15 downto 12), b(15 downto 12), open, open, open,
+	c3: four_bit_comparator port map (a(15 downto 12), b(15 downto 12), open, open, open,
 		a3_b3_eq, a3_b3_gt, a3_b3_lt);
 
 	eq <= a3_b3_eq and a2_b2_eq and a1_b1_eq and a0_b0_eq;
