dbe5101e52e507f93de0a2b2335956ab494771bf
Parham Alvani <parham.alvani@gmail.com>
2016-03-28 21:06:13 +0430

    HW-2 problem 11 shift register entity was added.

---
+++ HW-2/src/p11/p11.vhd
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------
+-- Author:        Parham Alvani (parham.alvani@gmail.com)
+--
+-- Create Date:   28-03-2016
+-- Module Name:   p11.vhd
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity shift_register is
+	generic (N : integer := 32);
+	port (serial_in : in std_logic;
+		w_s : in std_logic;
+		clk : in std_logic;
+		serial_out : out std_logic;
+		parallel_in : in std_logic_vector (N - 1 downto 0);
+		parallel_out : out std_logic_vector (N - 1 downto 0));
+end entity shif_register;
