f65f608013112e0a3c9f0b443d0a7e5a9309b47e
Parham Alvani <parham.alvani@gmail.com>
2016-03-29 11:07:33 +0430

    HW-2 problem 10 was completed.

--- HW-2/src/p10/p10.vhd
+++ HW-2/src/p10/p10.vhd
@@ -17,26 +17,25 @@ entity squart is
 end squart;
 
 architecture rtl of squart is
-	signal result : std_logic_vector (N - 1 downto 0) := (others => '0');
-	signal mask : std_logic_vector (N - 1 downto 0) := (N - 2 => '1', others => '0');
+	signal result : std_logic_vector (N - 1 downto 0);
+	signal mask : std_logic_vector (N - 1 downto 0);
 begin
 	process(clk, data_in)
-		variable data_buff : std_logic_vector(N - 1 downto 0) := (others => '0');
+		variable data_buff : std_logic_vector(N - 1 downto 0);
 	begin
 		if data_in'event then
 			data_buff := data_in;
-			mask <= (N - 2 => '1', others => '0');
+			mask <= ((N - 2) => '1', others => '0');
 			result <= (others => '0');
-		end if;
-		if clk'event and clk = '1' then
+		elsif clk'event and clk = '1' then
 			if mask > data_in then
 				mask <= std_logic_vector(shift_right(unsigned(mask), 2));
 			elsif unsigned(mask) /= 0 then
 				if data_buff >= result + mask then
 					data_buff := data_buff - (result + mask);
-					result <= std_logic_vector(shift_left(unsigned(result), 1)) + mask;
+					result <= std_logic_vector(shift_right(unsigned(result), 1)) + mask;
 				else
-					result <= std_logic_vector(shift_left(unsigned(result), 1));
+					result <= std_logic_vector(shift_right(unsigned(result), 1));
 				end if;
 				mask <= std_logic_vector(shift_right(unsigned(mask), 2));
 			else
