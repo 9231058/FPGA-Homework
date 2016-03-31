95e69562cafe114338ffaa761ac2bfa93cb0aa2a
Parham Alvani <parham.alvani@gmail.com>
2016-03-31 09:47:40 +0430

    Main function in concurrent way was added.

---
+++ Project-Phase1/src/main-concurrent.vhd
@@ -0,0 +1,30 @@
+--------------------------------------------------------------------------------
+-- Author:        Parham Alvani (parham.alvani@gmail.com)
+--
+-- Create Date:   31-03-2016
+-- Module Name:   main-concurrent.vhd
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity main_concurrent is
+	port (c1, c2 : in character;
+		a, b : out std_logic_vector (4 downto 0));
+end entity main_concurrent;
+
+architecture rtl of main_concurrent is
+begin
+	process (c)
+	begin
+		if c1 = ' ' then
+			a <= "11010"; -- a = 26
+		else
+			a <= std_logic_vector(to_unsigned(character'pos(c1) - 96, 5)); -- a = s[i] - 'a'
+		end if;
+		if c2 = ' ' then
+			b <= "11010"; -- b = 26
+		else
+			b <= std_logic_vector(to_unsigned(character'pos(c2) - 96, 5)); -- b = s[i + 1] - 'a'
+		end if;
+	end process;
+end architecture rtl;
