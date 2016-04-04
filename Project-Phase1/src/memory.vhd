75b828abd31655d749b9f871f0707dca4d73ccca
Parham Alvani <parham.alvani@gmail.com>
2016-04-04 14:20:13 +0430

    Memory chip select replaced with clock

--- Project-Phase1/src/memory.vhd
+++ Project-Phase1/src/memory.vhd
@@ -12,17 +12,17 @@ entity memory is
 	port (address : in std_logic_vector;
 		data_in : in std_logic_vector;
 		data_out : out std_logic_vector;
-		cs, rwbar : in std_logic);
+		clk, rwbar : in std_logic);
 end entity memory;
 
 architecture behavioral of memory is
 	type mem is array (natural range <>, natural range <>) of std_logic;
 begin
-	process (cs, rwbar, address, data_in)
+	process (clk)
 		constant memsize : integer := 2 ** address'length;
 		variable memory : mem (0 to memsize - 1, data_in'range);
 	begin
-		if cs = '1' then -- Chip Select :)
+		if  clk'event and clk = '1' then
 			if rwbar = '1' then -- Readiing :)
 				for i in data_out'range loop
 					data_out(i) <= memory (to_integer(unsigned(address)), i);
