bac08523b78c63831ad9f258613e6f2d57f645e7
Parham Alvani <parham.alvani@gmail.com>
2016-04-23 21:57:17 +0430

    Controller issues were corrected.

--- Project-Phase1/src/sequential/controller.vhd
+++ Project-Phase1/src/sequential/controller.vhd
@@ -11,10 +11,10 @@ entity controller is
 	port (clk, reset : in std_logic;
 		fitness_reset, fitness_clk : out std_logic;
 		fitness_done : in std_logic;
-		memory_rwbar : out std_logic);
+		memory_rwbar, memory_en : out std_logic);
 end entity controller;
 
-architecture controller is
+architecture rtl of controller is
 	type state is (reset, fitness_process, memory_read, memory_increase, memory_write);
 	signal next_state, current_state : state;
 begin
@@ -30,18 +30,20 @@ begin
 		end if;
 	end process;
 	-- outputs
-	process (current_state, fitness_done)
+	process (current_state)
 	begin
 		if current_state = reset then
 			fitness_reset <= '1';
 			fitness_clk = '0';
+			memory_en = '0';
 		elsif current_state = fitness_process then
-			fitness_clk = '1';
+			if fitness_done = '0' then
+				fitness_clk = '1';
+			end if;
+			memory_en = '0';
 		elsif current_state = memory_read then
 			memory_rwbar = '1';
 			fitness_clk = '0';
-		elsif current_state = increase then
-			-- ?
 		elsif current_state = memory_write then
 			memory_rwbar = '0';
 		end if;
@@ -52,7 +54,11 @@ begin
 		if current_state = reset then
 			next_state = fitness_process;
 		elsif current_state = fitness_process then
-			next_state = memory_read;
+			if fitness_done = '0' then
+				next_state = memory_read;
+			else
+				next_state = reset;
+			end if;
 		elsif current_state = memory_read then
 			next_state = increase;
 		elsif current_state = increase then
@@ -61,3 +67,4 @@ begin
 			next_state = fitness_process;
 		end if;
 	end process;
+end architecture rtl;
