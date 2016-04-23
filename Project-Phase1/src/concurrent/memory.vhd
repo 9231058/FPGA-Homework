120d72e9d751034f0768b42ba581589d930719b4
Parham Alvani <parham.alvani@gmail.com>
2016-04-23 22:41:35 +0430

    Add enable into memory module.

--- Project-Phase1/src/concurrent/memory.vhd
+++ Project-Phase1/src/concurrent/memory.vhd
@@ -12,7 +12,7 @@ entity memory is
 	port (address : in std_logic_vector;
 		data_in : in std_logic_vector;
 		data_out : out std_logic_vector;
-		clk, rwbar : in std_logic);
+		clk, rwbar, en : in std_logic);
 end entity memory;
 
 architecture behavioral of memory is
@@ -23,11 +23,11 @@ begin
 		variable memory : mem (0 to memsize - 1, data_in'range);
 	begin
 		if  clk'event and clk = '1' then
-			if rwbar = '1' then -- Readiing :)
+			if rwbar = '1' and en = '1' then -- Readiing :)
 				for i in data_out'range loop
 					data_out(i) <= memory (to_integer(unsigned(address)), i);
 				end loop;
-			else -- Writing :)
+			elsif rwbar = '0' and en = '1' then -- Writing :)
 				for i in data_in'range loop
 					memory (to_integer(unsigned(address)), i) := data_in (i);
 				end loop;
