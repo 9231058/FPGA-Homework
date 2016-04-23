8af0eb44c94906f43a62caa7b0ed81ce1ed60346
Parham Alvani <parham.alvani@gmail.com>
2016-04-23 22:37:11 +0430

    Controller compile errors were corrected.

--- Project-Phase1/src/sequential/controller.vhd
+++ Project-Phase1/src/sequential/controller.vhd
@@ -15,7 +15,7 @@ entity controller is
 end entity controller;
 
 architecture rtl of controller is
-	type state is (reset, fitness_process, memory_read, memory_increase, memory_write);
+	type state is (rst, fitness_process, memory_read, increase, memory_write);
 	signal next_state, current_state : state;
 begin
 	-- next
@@ -23,48 +23,48 @@ begin
 	begin
 		if clk'event and clk = '1' then
 			if reset = '1' then
-				current_state = reset;
+				current_state <= rst;
 			else
-				current_state = next_state;
+				current_state <= next_state;
 			end if;
 		end if;
 	end process;
 	-- outputs
 	process (current_state)
 	begin
-		if current_state = reset then
+		if current_state = rst then
 			fitness_reset <= '1';
-			fitness_clk = '0';
-			memory_en = '0';
+			fitness_clk <= '0';
+			memory_en <= '0';
 		elsif current_state = fitness_process then
 			if fitness_done = '0' then
-				fitness_clk = '1';
+				fitness_clk <= '1';
 			end if;
-			memory_en = '0';
+			memory_en <= '0';
 		elsif current_state = memory_read then
-			memory_rwbar = '1';
-			fitness_clk = '0';
+			memory_rwbar <= '1';
+			fitness_clk <= '0';
 		elsif current_state = memory_write then
-			memory_rwbar = '0';
+			memory_rwbar <= '0';
 		end if;
 	end process;
 	-- next_state
 	process (current_state, fitness_done)
 	begin
-		if current_state = reset then
-			next_state = fitness_process;
+		if current_state = rst then
+			next_state <= fitness_process;
 		elsif current_state = fitness_process then
 			if fitness_done = '0' then
-				next_state = memory_read;
+				next_state <= memory_read;
 			else
-				next_state = reset;
+				next_state <= rst;
 			end if;
 		elsif current_state = memory_read then
-			next_state = increase;
+			next_state <= increase;
 		elsif current_state = increase then
-			next_state = memory_write;
+			next_state <= memory_write;
 		elsif current_state = memory_write then
-			next_state = fitness_process;
+			next_state <= fitness_process;
 		end if;
 	end process;
 end architecture rtl;
