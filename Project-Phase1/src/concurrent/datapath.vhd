8ad1aa1eb080d14be157950d6977353427f42c0a
Parham Alvani <parham.alvani@gmail.com>
2016-04-24 01:13:32 +0430

    Datapath compile errors were corrected.

--- Project-Phase1/src/concurrent/datapath.vhd
+++ Project-Phase1/src/concurrent/datapath.vhd
@@ -6,6 +6,7 @@
 --------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity datapath is
 	port (clk : in std_logic;
@@ -29,16 +30,18 @@ architecture rtl of datapath is
 	end component;
 	component fitness
 		port (c1, c2 : in character;
-			a, b : out std_logic_vector (4 downto 0));
+			a, b : out std_logic_vector(4 downto 0));
 	end component;
 
 	for all:memory use entity work.memory;
 	for all:fulladdr use entity work.fulladdr;
 	for all:fitness use entity work.fitness;
+	
+	type fit_output is array (1 to 120) of std_logic_vector(4 downto 0);
 
 	signal data_in, data_out : std_logic_vector(3 downto 0);
 	signal c_out : std_logic;
-	signal a, b : array (1 to 120) of std_logic_vector(4 downto 0);
+	signal a, b : fit_output;
 	signal address : std_logic_vector(9 downto 0);
 begin
 	fa:fulladdr port map (data_out, "0001", '0', c_out, data_in);
@@ -46,5 +49,5 @@ begin
 	fits: for I in 1 to 119 generate
 		fit:fitness port map (str(I), str(I + 1), a(I), b(I));
 	end generate;
-	address <= a(sel) & b(sel);
+	address <= a(to_integer(unsigned(sel))) & b(to_integer(unsigned(sel)));
 end architecture rtl;
