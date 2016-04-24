5fa93d9efabb483e1de17076a6e51ba8b1fa6a62
Parham Alvani <parham.alvani@gmail.com>
2016-04-25 01:29:21 +0430

    HW-3, p5 shift register was added.

---
+++ HW-3/src/p5/shitf-register.vhd
@@ -0,0 +1,33 @@
+--------------------------------------------------------------------------------
+-- Author:        Parham Alvani (parham.alvani@gmail.com)
+--
+-- Create Date:   25-04-2016
+-- Module Name:   shitf-register.vhd
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity shift_register is
+	generic (N : integer := 8);
+	port (data_in : in std_logic_vector (N - 1 downto 0);
+		load, clk : in std_logic;
+		data_out : out std_logic);
+end entity;
+
+architecture rtl of shift_register is
+	signal reg : std_logic_vector (N - 1 downto 0);
+begin
+	process (clk)
+		variable I : integer;
+	begin
+		if clk'event and clk = '1' then
+			if load = '1' then
+				reg <= data_in;
+				I := 0;
+			else
+				data_out <= reg(I);
+				I := I + 1;
+			end if;
+		end if;
+	end process;
+end architecture rtl;
