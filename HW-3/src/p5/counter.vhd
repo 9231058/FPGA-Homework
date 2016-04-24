7af5f4e176fcbb39310917d715a98a9ca2080e0b
Parham Alvani <parham.alvani@gmail.com>
2016-04-25 02:40:37 +0430

    Counter module was improved.

--- HW-3/src/p5/counter.vhd
+++ HW-3/src/p5/counter.vhd
@@ -20,8 +20,8 @@ begin
 		variable count_buff : std_logic_vector (N - 1 downto 0) := (others => '0');
 	begin
 		if clk'event and clk = '1' then
-			count_buff := count_buff + '1';
 			count <= count_buff;
+			count_buff := count_buff + '1';
 		end if;
 		if reset = '1' then
 			count_buff := (others => '0');
