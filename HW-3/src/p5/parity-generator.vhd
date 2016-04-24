cd48743abca0aeda305272d49c37feb87e4d3a52
Parham Alvani <parham.alvani@gmail.com>
2016-04-25 01:59:43 +0430

    HW-3 p5 parity generator was added.

---
+++ HW-3/src/p5/parity-generator.vhd
@@ -0,0 +1,55 @@
+--------------------------------------------------------------------------------
+-- Author:        Parham Alvani (parham.alvani@gmail.com)
+--
+-- Create Date:   25-04-2016
+-- Module Name:   parity-generator.vhd
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164;
+
+entity parity_generator is
+	port (w, clk, reset : in std_logic;
+		p : out std_logic);
+end entity parity_generator;
+
+architecture rtl of parity_generator is
+	type state is (even, odd);
+	signal current_state, next_state : state;
+begin
+	process (clk)
+	begin
+		if clk'event and clk = '1' then
+			if reset = '1' then
+				current_state = even;
+			else
+				current_state <= next_state;
+			end if;
+		end if;
+	end process;
+
+	process (current_state, w)
+	begin
+		if current_state = even then
+			if w = '1' then
+				next_state = odd;
+			else
+				next_state = even;
+			end if;
+		else
+			if w = '1' then
+				next_state = even;
+			else
+				next_state = odd;
+			end if;
+		end if;
+	end process;
+
+	process (current_state)
+	begin
+		if current_state = even then
+			p <= '0';
+		else
+			p <= '1';
+		end if;
+	end process;
+end architecture rtl;
