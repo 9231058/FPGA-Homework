aeb8dea927526d80c816ae5adfa8b58ede0d8509
Parham Alvani <parham.alvani@gmail.com>
2016-05-06 11:15:37 +0430

    Vending machine code was completed.

--- HW-4/src/p9/vending-machine.vhd
+++ HW-4/src/p9/vending-machine.vhd
@@ -6,6 +6,7 @@
 --------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
 
 entity vending_machine is
 	port(coin_in : in std_logic;
@@ -19,3 +20,50 @@ entity vending_machine is
 		coin_return_10 : out std_logic_vector(7 downto 0);
 		coin_return_100 : out std_logic_vector(7 downto 0));
 end entity;
+
+architecture rtl of vending_machine is
+	signal coin_in_total : std_logic_vector(7 downto 0);
+begin
+	process (coin_in)
+	begin
+		if coin_in = '1' then
+			coin_return <= '0';
+			if coin_in_1 = '1' and coin_in_10 = '0' and coin_in_100 = '0' then
+				coin_in_total = coin_in_total + "00000001";
+			elsif coin_in_1 = '0' and coin_in_10 = '1' and coin_in_100 = '0' then
+				coin_in_total = coin_in_total + "00001010";
+			elsif coin_in_1 = '0' and coin_in_10 = '0' and coin_in_100 = '1' then
+				coin_in_total = coin_in_total + "01100100";
+			end if;
+		end if;
+	end process;
+	process (buy_in)
+		variable coin_return_total : std_logic_vector(7 downto 0);
+		variable coin_return_100_var : std_logic_vector(7 downto 0);
+		variable coin_return_10_var : std_logic_vector(7 downto 0);
+		variable coin_return_1_var : std_logic_vector(7 downto 0);
+	begin
+		if buy_in = '1' then
+			coin_return_total := coin_in_total - price;
+			coin_return_100_var := "00000000";
+			coin_return_10_var := "00000000";
+			coin_return_1_var := "00000000";
+			while coin_return_total > "01100100" loop
+				coin_return_total := coin_return_total - "01100100";
+				coin_return_100_var := coin_return_100_var + "00000001";
+			end loop;
+			while coin_return_total > "00001010" loop
+				coin_return_total := coin_return_total - "00001010";
+				coin_return_10_var := coin_return_10_var + "00000001";
+			end loop;
+			while coin_return_total > "00000001" loop
+				coin_return_total := coin_return_total - "00000001";
+				coin_return_1_var := coin_return_1_var + "00000001";
+			end loop;
+			coin_return <= '1';
+			coin_return_1 <= coin_return_1_var;
+			coin_return_10 <= coin_return_10_var;
+			coin_return_100 <= coin_return_100_var;
+		end if;
+	end process;
+end architecture;
