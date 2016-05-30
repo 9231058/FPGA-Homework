c6dea98a492474986600a8738fec97ef00883648
Parham Alvani <parham.alvani@gmail.com>
2016-05-31 00:06:42 +0430

    Datapath module was corrected.

--- HW-6/src/p8/datapath.vhd
+++ HW-6/src/p8/datapath.vhd
@@ -9,17 +9,11 @@ use IEEE.std_logic_1164.all;
 
 
 entity datapath is
-	port (clk, a, b : in std_logic;
+	port (clk, ent, ext : in std_logic;
 	     output : out std_logic_vector (3 downto 0));
 end entity;
 
-architecture rtl of datapath is
-	component control
-		port (ent, ext : out std_logic
-			a, b : in std_logic;
-			clk : in std_logic);
-	end component;
-	
+architecture rtl of datapath is	
 	component counter
 		generic (N : integer := 4);
 		port (inc, dec : in std_logic;
@@ -27,11 +21,7 @@ architecture rtl of datapath is
 			clk : in std_logic);
 	end component;
 
-	for all:control use entity work.control(rtl);
 	for all:counter use entity work.counter;
-
-	signal ent, ext : std_logic;
 begin
-	ctrl : control port map (ent, ext, a, b, clk);
 	cntr : counter port map (ent, ext, output, clk);
 end architecture;
