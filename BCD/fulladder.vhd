140a729a2bf488a905dd860813a0d98d858da4e7
Parham Alvani <parham.alvani@gmail.com>
2017-04-15 05:06:01 +0430

    BCD fulladder was improved.

--- BCD/fulladder.vhd
+++ BCD/fulladder.vhd
@@ -1,13 +1,13 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity fulladdr is
+entity fulladder is
 	port(a, b, c_in : in std_logic;
 		sum, c_out : out std_logic);
-end entity fulladdr;
+end entity fulladder;
 
-architecture arch_fulladdr of fulladdr is
+architecture structeral of fulladder is
 begin
 	sum <= a xor b xor c_in;
 	c_out <= (a and b) or (a and c_in) or (b and c_in);
-end architecture arch_fulladdr;
+end architecture;
