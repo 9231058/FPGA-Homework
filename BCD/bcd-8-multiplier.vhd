8238f23dce9b6bc39b5698ab2121369827c2947d
Parham Alvani <parham.alvani@gmail.com>
2017-04-17 21:42:03 +0430

    BCD multiplier was improved.

--- BCD/bcd-8-multiplier.vhd
+++ BCD/bcd-8-multiplier.vhd
@@ -14,8 +14,10 @@ architecture structeral of bcd_eight_multiplier is
 	end component;
 
 	component bcd_adder is
-		port (a, b: in std_logic_vector (3 downto 0);
-			v: out std_logic_vector (7 downto 0));
+		port (a : in std_logic_vector (7 downto 0);
+			b: in std_logic_vector (3 downto 0);
+			res: out std_logic_vector (3 downto 0);
+			cout: out std_logic_vector (3 downto 0));
 	end component;
 
 	signal sum : std_logic_vector (31 downto 0);
@@ -26,6 +28,6 @@ begin
 
 	M: for I in 0 to 7 generate
 		ms: bcd_multiplier port map (a => a(I * 4 + 3 downto I * 4), b => b, v(7 downto 4) => c(I * 4 + 7 downto I * 4 + 4), v(3 downto 0) => sum(I * 4 + 3 downto I * 4));
-		as: bcd_adder port map (sum(I * 4 + 3 downto I * 4), c(I * 4 + 3 downto I * 4), v(I * 4 + 3 downto I * 4));
+		as: bcd_adder port map ("0000" & sum(I * 4 + 3 downto I * 4), c(I * 4 + 3 downto I * 4), v(I * 4 + 3 downto I * 4), open);
 	end generate M;
 end architecture;
