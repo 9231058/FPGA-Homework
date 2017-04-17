7ddd3621fe1d125e163624dbc32d1f9c5509c086
Parham Alvani <parham.alvani@gmail.com>
2017-04-17 22:05:53 +0430

    8 multiplier was corrected.

--- BCD/bcd-8-multiplier.vhd
+++ BCD/bcd-8-multiplier.vhd
@@ -10,7 +10,7 @@ end entity;
 architecture structeral of bcd_eight_multiplier is
 	component bcd_multiplier is
 		port (a, b : in std_logic_vector (3 downto 0);
-			v : out std_logic_vector (7 downto 0));
+			r : out std_logic_vector (7 downto 0));
 	end component;
 
 	component bcd_adder is
@@ -27,7 +27,7 @@ begin
 	v (35 downto 32) <= c (35 downto 32);
 
 	M: for I in 0 to 7 generate
-		ms: bcd_multiplier port map (a => a(I * 4 + 3 downto I * 4), b => b, v(7 downto 4) => c(I * 4 + 7 downto I * 4 + 4), v(3 downto 0) => sum(I * 4 + 3 downto I * 4));
+		ms: bcd_multiplier port map (a => a(I * 4 + 3 downto I * 4), b => b, r(7 downto 4) => c(I * 4 + 7 downto I * 4 + 4), r(3 downto 0) => sum(I * 4 + 3 downto I * 4));
 		as: bcd_adder port map ("0000" & sum(I * 4 + 3 downto I * 4), c(I * 4 + 3 downto I * 4), v(I * 4 + 3 downto I * 4), open);
 	end generate M;
 end architecture;
